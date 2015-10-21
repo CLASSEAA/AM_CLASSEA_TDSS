016dae4255b1c421f6fb92372d7ed887eda0cdcb
sourcevitor <vitorcosta-pm@hotmail.com>
2015-10-21 21:23:10 -0200

    VITOR 1.2

--- Desenvolvimento/AM_2015_/src/br/com/tdss/am/classea/dao/impl/OracleReservaDAO.java
+++ Desenvolvimento/AM_2015_/src/br/com/tdss/am/classea/dao/impl/OracleReservaDAO.java
@@ -17,8 +17,6 @@ public class OracleReservaDAO implements ReservaDAO{
 	Connection conn;
 	PreparedStatement stmt;
 	private static final String sqlConsultaReserva = "SELECT R.ID_RESERVA, P.ID_PESSOA, TO_CHAR(R.DT_SOLICITACAO,'DD/MM/YYYY'), TO_CHAR(R.DT_INICIO_RESERVA,'DD/MM/YYYY'), TO_CHAR(R.DT_FINAL_RESERVA,'DD/MM/YYYY'), R.QT_ADULTO, R.QT_CRIANCA, R.ST_RESERVA FROM T_AM_CLA_RESERVA R INNER JOIN T_AM_CLA_CLIENTE C ON(R.ID_CLIENTE = C.ID_CLIENTE) INNER JOIN T_AM_CLA_PESSOA P ON(C.ID_CLIENTE = P.ID_PESSOA) WHERE R.ID_RESERVA = ? ";
-	private static final String sqlConsultaCliente = "";
-	private static final String sqlConsultaQuarto = "";
 	private ResultSet rs;
 	private Reserva reserva;
 	private Cliente cliente;
@@ -39,9 +37,12 @@ public class OracleReservaDAO implements ReservaDAO{
 			this.reserva = new Reserva(); //Instanciando uma reserva
 			cliente = new Cliente(); //Instanciando uma cliente
 			funcionario = new Funcionario();
+			
+			
 			while(rs.next()){
 				this.reserva.setIdReserva(rs.getInt("ID_RESERVA"));
-				this.reserva.setCliente((rs.getInt("ID_PESSOA")); //pegando o id do cliente vindo da reserva e atribuindo ao cliente já instanciado
+				//this.reserva.
+				//cliente.setIdPessoa(rs.getInt("ID_PESSOA")); //pegando o id do cliente vindo da reserva e atribuindo ao cliente já instanciado
 				this.reserva.setDataSolicitacao(rs.getString("TO_CHAR(R.DT_SOLICITACAO,'DD/MM/YYYY')"));
 				this.reserva.setDataInicio(rs.getString("TO_CHAR(R.DT_INICIO_RESERVA,'DD/MM/YYYY')"));
 				this.reserva.setDataFinal(rs.getString("TO_CHAR(R.DT_FINAL_RESERVA,'DD/MM/YYYY')"));
@@ -50,10 +51,9 @@ public class OracleReservaDAO implements ReservaDAO{
 				this.reserva.setStatusReserva(rs.getBoolean("ST_RESERVA"));
 			}
 			
-			cliente = DaoFactory.getClienteDao().buscarCliente(cliente);
 			this.reserva.setCliente(cliente);
 			
-			
+			//this.reserva.setFuncionario(rs.get);
 			
 			return this.reserva;
 			
