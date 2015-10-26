03f4f093ae5c381aeb02de839c218ec8a09eccee
DouglasCarvalhoRomano <douglascarvalhoromano@gmail.com>
2015-10-26 20:06:35 -0200

    VoltandoAoNormal
    Teste

--- Desenvolvimento/AM_2015_/src/br/com/tdss/am/classea/servlet/RegistraHospedagem.java
+++ Desenvolvimento/AM_2015_/src/br/com/tdss/am/classea/servlet/RegistraHospedagem.java
@@ -7,33 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
 /**
  * Servlet implementation class RegistraHospedagem
  */
 @WebServlet("/RegistraHospedagem")
 public class RegistraHospedagem extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegistraHospedagem() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
+		System.out.println("AI VITOR VAI TOMA NO CÚ");
 	}
 
 }
