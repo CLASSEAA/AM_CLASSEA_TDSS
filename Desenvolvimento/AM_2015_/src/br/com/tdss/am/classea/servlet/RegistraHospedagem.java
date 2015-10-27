17c555d0ecb4f10adecd310dc5db5f5c8536e396
sourcevitor <vjose@DESKTOP-OEOPRKK.TrideaConsulting.local>
2015-10-27 19:27:18 -0200

    Vitor 1.4.3.1
    RegistraHospedagem: Progredindo
    
    Substituido OracleHospedagemDAO por HospedagemBO

--- Desenvolvimento/AM_2015_/src/br/com/tdss/am/classea/servlet/RegistraHospedagem.java
+++ Desenvolvimento/AM_2015_/src/br/com/tdss/am/classea/servlet/RegistraHospedagem.java
@@ -1,8 +1,6 @@
 package br.com.tdss.am.classea.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,15 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.tdss.am.classea.dao.impl.OracleHospedagemDao;
-import br.com.tdss.am.classea.dao.impl.OracleReservaDAO;
-import br.com.tdss.am.classea.entity.Funcionario;
+import br.com.tdss.am.classea.bo.HospedagemBO;
+import br.com.tdss.am.classea.bo.ReservaBO;
 import br.com.tdss.am.classea.entity.Hospedagem;
-import br.com.tdss.am.classea.entity.Quarto;
 import br.com.tdss.am.classea.entity.Reserva;
 import br.com.tdss.am.classea.utils.Util;
-
-import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
 
 /**
  * Servlet implementation class RegistraHospedagem
@@ -38,31 +32,34 @@ public class RegistraHospedagem extends HttpServlet {
 			String data = util.buscarDataAtual();
 			data = data.replace("/", "");
 			Hospedagem hospedagem = new Hospedagem();
-			OracleHospedagemDao hospedagemDAO = new OracleHospedagemDao();
+			HospedagemBO hospedagemBO = new HospedagemBO();
+
 			// Recebendo idReserva e buscando reserva a partir deste ID recebido
 			int idReserva = Integer.parseInt(request.getParameter("idReserva"));
 			Reserva reserva = new Reserva();
 			reserva.setIdReserva(idReserva);
-			OracleReservaDAO reservaDAO = new OracleReservaDAO();
-			
-			for (int i = 0; i < reservaDAO.buscarReservar(reserva).getQuartos()
+			ReservaBO reservaBO = new ReservaBO();
+
+			for (int i = 0; i < reservaBO.buscarReserva(reserva).getQuartos()
 					.size(); i++) {
-				hospedagem.setReserva(reservaDAO.buscarReservar(reserva));
+				hospedagem.setReserva(reservaBO.buscarReserva(reserva));
 				hospedagem.setFuncionario(reserva.getFuncionario());
-				hospedagem.setQuarto(reservaDAO.buscarReservar(reserva).getQuartos().get(i));
+				hospedagem.setQuarto(reservaBO.buscarReserva(reserva)
+						.getQuartos().get(i));
 				hospedagem.setDataEntrada(data);
-				
-				//Incluir no banco
-				hospedagemDAO.incluirHospedagem(hospedagem, hospedagem.getFuncionario());
-				
+
+				// Incluir no banco
+				hospedagemBO.incluirHospedagem(hospedagem,
+						hospedagem.getFuncionario());
+
 			}
 			System.out.println("aee");
 			System.out.println("Concluído");
 			response.sendRedirect("hospedagem.jsp?hospedagem=true");
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			response.sendRedirect("hospedagem.jsp?hospedagem=false");
 			e.printStackTrace();
-			}
 		}
+	}
 
 }
